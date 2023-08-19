8d35c829ece3bc00c38f4d6f157d7cb76230c923
Marat Sazanov <msazanov@gmail.com>
2023-08-19 20:37:57 +0400

    Update zabToJSON.py

--- zabToJSON.py
+++ zabToJSON.py
@@ -9,8 +9,15 @@ filename = sys.argv[1]
 
 with open(filename, 'r') as f:
     data = json.loads(f.read())['data']
-    decoded_data = base64.b64decode(data).decode('utf-8')
-    formatted_data = json.dumps(json.loads(decoded_data), indent=4, sort_keys=True)
+    
+    # Декодирование данных с обработкой ошибок
+    decoded_data = base64.b64decode(data).decode('utf-8', errors='replace')
+    
+    try:
+        formatted_data = json.dumps(json.loads(decoded_data), indent=4, sort_keys=True)
+    except json.JSONDecodeError as e:
+        print(f"Ошибка при преобразовании декодированных данных в JSON: {e}")
+        sys.exit(1)
 
     # Save the formatted data to a file with the same name but .json extension
     temp_output_filename = os.path.splitext(filename)[0] + ".json"
